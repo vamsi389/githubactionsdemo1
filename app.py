0ba1f8e8d07a4757a4e074397cf1e53ecbfb775f
vamsi389 <62508560+vamsi389@users.noreply.github.com>
2026-09-06 14:52:51 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+def add(a, b):
+    return a + b
+
+def multiply(a, b):
+    return a * b
+
+if __name__ == "__main__":
+    print("Addition:", add(2, 3))
+    print("Multiplication:", multiply(4, 5))
